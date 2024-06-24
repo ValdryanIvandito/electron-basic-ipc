e9e118a74d0e2e3c4020c904615ea0adcd67c071
ValdryanIvandito <valdryan08@gmail.com>
2024-06-24 16:48:31 +0700

    first commit

---
+++ src/preload/index.js
@@ -0,0 +1,19 @@
+import { contextBridge, ipcRenderer } from 'electron'
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+})
+
+contextBridge.exposeInMainWorld('openNewWindow', {
+  openUrl: (url) => {
+    ipcRenderer.send('openUrl', url)
+  }
+})
+
+contextBridge.exposeInMainWorld('openFileApi', {
+  openFile: () => {
+    return ipcRenderer.invoke('open-file')
+  }
+})
---
+++ src/renderer/src/renderer.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const chrome = document.getElementById('chrome')
+const node = document.getElementById('node')
+const electron = document.getElementById('electron')
+// const appPath = document.getElementById('app-path')
+const inputArea = document.getElementById('input-area')
+const submitButton = document.getElementById('url-submit')
+
+chrome.innerHTML += ' ' + versions.chrome()
+node.innerHTML += ' ' + versions.node()
+electron.innerHTML += ' ' + versions.electron()
+
+submitButton.addEventListener('click', () => {
+  const url = inputArea.value
+  if (url.trim() !== '') {
+    openNewWindow.openUrl(url)
+  }
+})
+
+const fileSelector = document.getElementById('file-select')
+const filePath = document.getElementById('file-path')
+
+fileSelector.addEventListener('click', async () => {
+  const path = await openFileApi.openFile()
+  filePath.innerHTML += path
+})
